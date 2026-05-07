72f8bc3f4f5fa6b586d17b3540da20f89ef3900c
tbeaumont79 <tbeaumont79@icloud.com>
2026-05-07 11:57:50 +0200

    feat(config): adding phpstan

---
+++ tests/console-application.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Symfony\Component\Dotenv\Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+$kernel = new App\Kernel('dev', true);
+$kernel->boot();
+
+return new Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
