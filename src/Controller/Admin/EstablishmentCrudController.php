13fcac559975a1ce630a75922d7d9770ac4624ed
tbeaumont79 <tbeaumont79@icloud.com>
2026-05-09 11:46:59 +0200

    feat(admin): EasyAdmin Crud controllers + dashboard

---
+++ src/Controller/Admin/EstablishmentCrudController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\Main\Establishment;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+
+/**
+ * @extends AbstractCrudController<Establishment>
+ */
+class EstablishmentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Establishment::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('name'),
+            IntegerField::new('tenantId'),
+            TextField::new('address'),
+            AssociationField::new('user'),
+        ];
+    }
+}
