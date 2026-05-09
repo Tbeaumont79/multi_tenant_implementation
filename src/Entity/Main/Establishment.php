83af8c01e36468ff4ad85288c4399de378b459af
tbeaumont79 <tbeaumont79@icloud.com>
2026-05-09 11:59:50 +0200

    fix(config): set APP_SECRET, restore session CSRF for EasyAdmin

--- src/Entity/Main/Establishment.php
+++ src/Entity/Main/Establishment.php
@@ -112,4 +112,9 @@ class Establishment
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->email ?? '';
+    }
 }
--- src/Entity/Main/User.php
+++ src/Entity/Main/User.php
@@ -176,4 +176,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function __toString(): string
+    {
+        return $this->email ?? '';
+    }
 }
